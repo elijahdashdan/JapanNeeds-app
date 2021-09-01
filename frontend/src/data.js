942e95ca4b1a7f6b9271b55aff644df4bc8c6825
DM <dm@DM.local>
2021-09-01 13:11:05 +0900

    First commit

---
+++ frontend/src/data.js
@@ -0,0 +1,49 @@
+const data = {
+    products:[
+        {
+            _id:"1",
+            name: "Beauteen Terracotta Orange",
+            category: "Hair",
+            image: "/images/Beauteen Terracotta Orange.jpg",
+            price: 999,
+            brand: "Beauteen",
+            rating:4.5,
+            numReviews:10,
+            description: "High Quality product"
+        },
+        {
+            _id:"2",
+            name: "Biore Cleansing Liquid",
+            category: "Face",
+            image: "/images/Biore Cleansing Liquid.jpg",
+            price: 650,
+            brand: "Biore",
+            rating:4.5,
+            numReviews:10,
+            description: "High Quality product"
+        },
+        {
+            _id:"3",
+            name: "Biore Orange",
+            category: "Face",
+            image: "/images/Saborino1.jpg",
+            price: 250,
+            brand: "Biore",
+            rating:4.5,
+            numReviews:10,
+            description: "High Quality product"
+        },
+        {
+            _id:"4",
+            name: "Biore Perfect Oil",
+            category: "Face",
+            image: "/images/Biore Perfect Oil.jpg",
+            price: 599,
+            brand: "Biore",
+            rating:4.5,
+            numReviews:10,
+            description: "High Quality product"
+        }
+    ]
+}
+export default data;
