39ed60e316ea885a752027606b9cc2c48e817b2b
DM <dm@DM.local>
2021-10-07 14:32:44 +0900

    Added actions

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,9 +1,21 @@
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } from "../constants/productConstants"
+import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST } from "../constants/productConstants"
 import axios from "axios";
 
 export const listProducts = () => async(dispatch) =>{
     dispatch({
         type: PRODUCT_LIST_REQUEST
+    });
+    try {
+        const {data} = await axios.get('/api/products');
+        dispatch({type: PRODUCT_LIST_SUCCESS,payload: data});
+    } catch(error){
+        dispatch({type: PRODUCT_LIST_FAIL,payload: error.message});
+    }
+}
+
+export const detailsProduct = (productID) => async(dispatch) =>{
+    dispatch({
+        type: PRODUCT_DETAILS_REQUEST, payload: productID
     });
     try {
         const {data} = await axios.get('/api/products');
